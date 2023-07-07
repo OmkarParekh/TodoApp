90bdc988460392927e27e705469e63944ac3494a
Neosoft 1 <neosoft1@Omkars-MacBook-Pro.local>
2023-07-07 16:33:41 +0530

    Test 2

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Todo App",
+      title: "Todo App1",
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
