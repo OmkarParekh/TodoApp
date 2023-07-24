79f2ca615d0d7f949b9b35ae901c3677e5d5fba4
Neosoft 1 <neosoft1@Omkars-MacBook-Pro.local>
2023-07-24 12:12:19 +0530

    Hello

--- lib/Pages/dialog.dart
+++ lib/Pages/dialog.dart
@@ -10,59 +10,65 @@ class DialogTodo extends StatefulWidget {
 
 class _DialogTodoState extends State<DialogTodo> {
   Map data = {"Task": "", "Time": "", "Dropdown": "AT HOME", "Changed": false};
+
+  final formKey = GlobalKey<FormState>();
+  TextEditingController task = TextEditingController();
   @override
   Widget build(BuildContext context) {
     List option = ["AT OFFICE", "AT HOME", "AWAY"];
-    return AlertDialog(
-      title: const Text("Add Task"),
-      content: const Text("Add the Task for the List"),
-      actions: [
-        TextFormField(
-          decoration: const InputDecoration(hintText: "Task"),
-          onChanged: (value) => {
-            setState(() {
-              data["Task"] = value;
-              data['Time'] =
-                  "${DateTime.now().hour}:${DateTime.now().minute}:${DateTime.now().second}";
-              data['Changed'] = true;
-              // data = {
-              //   "Task": value,
-              //   "Time":
-              //       "${DateTime.now().hour}:${DateTime.now().minute}:${DateTime.now().second}"
-              // };
-            })
-          },
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        Row(
-          children: [
-            const Text("Location :- "),
-            DropdownButton(
-              value: data['Dropdown'],
-              // value: Text(data['Dropdown']),
-              items: option
-                  .map<DropdownMenuItem<String>>(
-                      (e) => DropdownMenuItem(value: e, child: Text(e)))
-                  .toList(),
-              onChanged: (value) => {
-                setState(() {
-                  data["Dropdown"] = value;
-                })
-              },
-            ),
-          ],
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        ElevatedButton(
-            onPressed: () {
-              Navigator.pop(context, data);
+    return Form(
+      key: formKey,
+      child: AlertDialog(
+        title: const Text("Add Task"),
+        content: const Text("Add the Task for the List"),
+        actions: [
+          TextFormField(
+            decoration: const InputDecoration(hintText: "Task"),
+            controller: task,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return ("Please Enter a Task");
+              }
+              return (null);
             },
-            child: const Text("Add Task")),
-      ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              const Text("Location :- "),
+              DropdownButton(
+                value: data['Dropdown'],
+                // value: Text(data['Dropdown']),
+                items: option
+                    .map<DropdownMenuItem<String>>(
+                        (e) => DropdownMenuItem(value: e, child: Text(e)))
+                    .toList(),
+                onChanged: (value) => {
+                  setState(() {
+                    data["Dropdown"] = value;
+                  })
+                },
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          ElevatedButton(
+              onPressed: () {
+                if (formKey.currentState!.validate()) {
+                  data['Changed'] = true;
+                  data['Task'] = task.text;
+                  data['Time'] =
+                      "${DateTime.now().hour}:${DateTime.now().minute}:${DateTime.now().second}";
+                  Navigator.pop(context, data);
+                }
+              },
+              child: const Text("Add Task")),
+        ],
+      ),
     );
   }
 }
